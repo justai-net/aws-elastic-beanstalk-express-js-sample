99035437595750b999e27df45fe777529c58a3b8
Adam Inthapanya <adam.int@live.com>
2022-03-23 14:07:34 +1100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Developing on AWS!'));
+app.get('/', (req, res) => res.send('Hello Developing on AWS! Now with a manual approval stage'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
